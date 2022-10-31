aacc3ee331007e43f1aef3137465c7e4e0576748
TheFrenchTechMan <95764063+TheFrenchTechMan@users.noreply.github.com>
2022-10-31 19:45:57 +0100

    Modified a bit the code

--- hasher.py
+++ hasher.py
@@ -5,21 +5,19 @@ _hashtable = hashlib
 hashtable = {}
 for index, value in _hashtable.__dict__.items():
     if callable(value):
-        print(f"Added {index} to hashtable")
         hashtable[index] = value
 
-correctalgo = False
-while not correctalgo:
+correctAlgorithm = False
+while not correctAlgorithm:
     algorithm = input("Enter an algorithm to encode with: ")
     if algorithm not in hashlib.algorithms_guaranteed:
-        print("Invalid algorithm! Please type a correct algorithm")
+        print("Invalid algorithm! Please type a correct algorithm.")
     else:
-        correctalgo = True
+        correctAlgorithm = True
 
 text = input("Enter text to encode with algorithm \"" + algorithm + "\": ")
-time.sleep(1)
+
 encodedtext = hashtable[algorithm](text.encode()).hexdigest()
 
-print(f"Your hash for the text \"{text}\" is:\n{encodedtext}")
-time.sleep(1)
+print(f"\nYour hash for the text \"{text}\" is:\n\n{encodedtext}\n")
 quit()
