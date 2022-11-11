030bb1e2e22ec617220a9570c416dd9d0b827798
TheFrechTechMan <massardier.camille@gmail.com>
2022-11-11 13:43:42 +0100

    Update hasher.py

--- hasher.py
+++ hasher.py
@@ -8,17 +8,19 @@ for index, value in _hashtable.__dict__.items():
     if callable(value):
         hashtable[index] = value
 
+print("")
 print("Available algorithms: " + algorithms)
+print("")
 
 correctAlgorithm = False
 while not correctAlgorithm:
-    algorithm = input("Enter an algorithm to encode with: ")
+    algorithm = input("Enter an algorithm to hash with: ")
     if algorithm not in hashlib.algorithms_guaranteed:
         print("Invalid algorithm! Please type a correct algorithm.")
     else:
         correctAlgorithm = True
 
-text = input("Enter text to encode with algorithm \"" + algorithm + "\": ")
+text = input("Enter text to hash with algorithm \"" + algorithm + "\": ")
 
 encodedtext = hashtable[algorithm](text.encode()).hexdigest()
 
